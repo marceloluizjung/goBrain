82d7b0bdfe4f87ef29ac76059886b7701502ed99
Marcelo <marcelo_4940@hotmail.com>
2019-03-23 17:25:07 -0300

    Chat Ok

--- Client.go
+++ Client.go
@@ -7,17 +7,33 @@ import "os"
 
 func main() {
 
-  // connect to this socket
-  conn, _ := net.Dial("tcp", "127.0.0.1:8091")
-  for { 
+    // connect to this socket
+    conn, _ := net.Dial("tcp", "127.0.0.1:8091")
+    for {   
+    
+    // will listen for message to process ending in newline (\n)
+    messaget, _ := bufio.NewReader(conn).ReadString('\n')
+    // output message received
+    fmt.Print("Message Received:", string(messaget))    
+
+    switch messaget {
+    case "1":
+      fmt.Println("1 Key Pressed")
+      break
+    case "2":
+      fmt.Println("2 Key Pressed")
+      break
+    }
+
+
     // read in input from stdin
     reader := bufio.NewReader(os.Stdin)
     fmt.Print("Text to send: ")
     text, _ := reader.ReadString('\n')
     // send to socket
     fmt.Fprintf(conn, text + "\n")
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
   }
+
+ 
+
 }
--- Server.go
+++ Server.go
@@ -3,8 +3,7 @@ package main
 import "net"
 import "fmt"
 import "bufio"
-import "strings" // only needed below for sample processing
-import "os/exec"
+import "os"
 
 func main() {
 
@@ -18,23 +17,24 @@ func main() {
 
   // run loop forever (or until ctrl-c)
   for {
-    // will listen for message to process ending in newline (\n)
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    // output message received
+
+  // read in input from stdin
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Print("Text to send: ")
+  text, _ := reader.ReadString('\n')
+  // send to socket
+  fmt.Fprintf(conn, text + "\n")
+    
+  // will listen for message to process ending in newline (\n)
+  message, _ := bufio.NewReader(conn).ReadString('\n')
+  // output message received
 	fmt.Print("Message Received:", string(message))
 	
 	//Command here
-	var entrada string = string(message)
-	var compare string = "exp"
-	fmt.Print("Teste Received:", entrada)
-	if (entrada == compare) {
-	fmt.Print("Your comand ok")
-	cmd := exec.Command(`explorer`, `/select,`, `C:\`)
-    cmd.Run()
-	}
-    // sample process for string received
-    newmessage := strings.ToUpper(message)
-    // send new string back to client
-    conn.Write([]byte(newmessage + "\n"))
+	
+
   }
+
+
+
 }
